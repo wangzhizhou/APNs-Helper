38509d0287aa79e37e15b12981fbca52af1dabeb
wangzhizhou <wangzhizhou@bytedance.com>
2023-05-13 22:32:00 +0800

    change the Doc Target to Dynamic Library to skip code sign

--- APNs Helper Doc/EmptyFile.swift
+++ APNs Helper Doc/EmptyFile.swift
@@ -1,7 +1,2 @@
-@main
-private struct Empty {
-    static func main() {
-        // This is an empty source file used to make 'APNs Helper Doc' a valid
-        // documentation build target.
-    }
-}
+// This is an empty source file used to make 'APNs Helper Doc' a valid
+// documentation build target.
