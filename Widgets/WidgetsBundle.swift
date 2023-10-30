fd952f22696d80bd0aa29c8a514c7633d2a6af17
wangzhizhou <wangzhizhou@bytedance.com>
2023-10-30 20:52:23 +0800

    add widget extension to tester app

---
+++ Widgets/WidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by joker on 2023/10/30.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        Widgets()
+    }
+}
