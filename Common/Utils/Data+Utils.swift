642c2cb074d9e3768a3986aeb837c50894b2af41
wangzhizhou <824219521@qq.com>
2024-07-11 02:14:10 +0800

    compile the two target

---
+++ Common/Utils/Data+Utils.swift
@@ -0,0 +1,17 @@
+//
+//  Data+Utils.swift
+//  APNs Helper
+//
+//  Created by joker on 2023/4/5.
+//
+
+import Foundation
+
+extension Data {
+    
+    var hexString: String { reduce("") { $0 + String(format: "%02x", $1) } }
+    
+    var toUTF8String: String { String(decoding: self, as: UTF8.self) }
+    
+    
+}
